f75948ef305b18942897bff2f069ff407fcbbbfd
Sangzh1 <Kangseobon@gmail.com>
2024-01-11 17:19:00 +0500

    file has been actived

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// 1 Misol
+let Boyi = +prompt (' Qiymatni kiriting!');
+let Baland = +prompt (' Qiymatni kiriting!');
+let Eni = +prompt ('Qiymatni kiriting!');
+let yigindi_1 = Boyi*Baland*Eni;
+console.log( yigindi_1);
+
+// 2 Misol
+let kub_ikki = +prompt(' Qiymatni kiriting!');
+let yigindi_2 = kub_ikki*kub_ikki*kub_ikki;
+console.log( yigindi_2);
+
+// 3 Misol
+let sonYigindisi_1 = +prompt(' Qiymatni kiritng!')
+let sonYigindisi_2 = +prompt (' Qiymatni kiriting!');
+let jami_yigindi_1 = sonYigindisi_1+sonYigindisi_2
+console.log( jami_yigindi_1**2);
+
+// 4 Misol
+let sonikki_on = +prompt('Qiymatni kiriting!')
+let yigindi_4 = Math.floor((sonikki_on % 100)/10);
+console.log( yigindi_4);
+
+// 5 Misol
+let son_ikkixona = +prompt('Qiymatni kiritng!')
+let yigindi_5 = son_ikkixona % 10
+let yigindi_6 = Math.floor(son_ikkixona  / 10)
+let yigindi_jami =yigindi_5 + yigindi_6
+console.log ( yigindi_jami);
+
+// 6  Misol
+let sonYuz = +prompt(' Qiymatni kiriting!')
+let yigindi_8 = sonYuz % 10
+let yigindi_9 = Math.floor(sonYuz / 100)
+console.log( yigindi_9);
+
+// 7 Misol
+let baland = +prompt(' Qiymatni kiriting!')
+let boyi = +prompt('Qiymatni kiriting!')
+let eni = +prompt(' Qiymatni kiriting!')
+let yigindi_10 = Baland + Boyi + Eni
+console.log(yigindi_10);
+
